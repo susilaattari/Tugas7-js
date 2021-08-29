b97683df7dbb54ea45d1398bc16f34d5bde2d182
susila-tropika <susilatropikanew@gmail.com>
2021-08-30 02:54:29 +0700

    menambahkan file tugas7.js

---
+++ tugas7.js
@@ -0,0 +1,10 @@
+const namaSiswa ="Hero"
+let tinggiBadan=[160,165,170,180,185,190];
+
+for (let i = 0 ; i<tinggiBadan.length; i++){
+    console.log(` Tinggi Badan ${namaSiswa} adalah ${tinggiBadan[i]} Cm`)
+}
+
+for(let i of tinggiBadan){
+    console.log(i);
+}
